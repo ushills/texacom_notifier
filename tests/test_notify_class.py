d97261a269a2c81baafe6526680d8f04052645ef
ushills <web@ushills.co.uk>
2019-06-05 16:25:30 +0100

    changed class of test to fake_signal

--- tests/test_notify_class.py
+++ tests/test_notify_class.py
@@ -18,77 +18,79 @@ import main
 
 
 """create an instance of Notifier 
-   called test_signal as a base for testing
+   called fake_signal as a base for testing
 """
 
 
 class TestNotifier:
     @pytest.fixture(scope="class")
-    def test_signal(self):
+    def fake_signal(self):
         return Notifier()
 
     @pytest.fixture(scope="class")
-    def set_action1(self, test_signal):
-        test_signal.set_action1("signal+activated")
-        assert test_signal.action1 == "signal+activated"
+    def set_action1(self, fake_signal):
+        fake_signal.set_action1("signal+activated")
+        assert fake_signal.action1 == "signal+activated"
 
     @pytest.fixture(scope="class")
-    def set_action2(self, test_signal):
-        test_signal.set_action2("signal+unactivated")
-        assert test_signal.action2 == "signal+unactivated"
+    def set_action2(self, fake_signal):
+        fake_signal.set_action2("signal+unactivated")
+        assert fake_signal.action2 == "signal+unactivated"
 
     @pytest.fixture(scope="class")
-    def set_signal_true(self, test_signal):
-        test_signal.check_signal(True)
+    def set_signal_true(self, fake_signal):
+        fake_signal.check_signal(1)
 
     @pytest.fixture(scope="class")
-    def set_signal_false(self, test_signal):
-        test_signal.check_signal(False)
+    def set_signal_false(self, fake_signal):
+        fake_signal.check_signal(0)
 
     def test_signal_true_gives_signal_is_active(
-        self, test_signal, set_action1, set_signal_true
+        self, fake_signal, set_action1, set_signal_true
     ):
-        assert test_signal.signal_is_active is True
+        assert fake_signal.signal_is_active is True
 
     def test_signal_false_gives_signal_is_not_active(
-        self, test_signal, set_action1, set_signal_false
+        self, fake_signal, set_action1, set_signal_false
     ):
-        assert test_signal.signal_is_active is False
+        assert fake_signal.signal_is_active is False
 
-    def test_create_url(self, test_signal):
+    def test_create_url(self, fake_signal):
         assert (
-            test_signal.create_url("test+action")
+            fake_signal.create_url("test+action")
             == "http://maker.ifttt.com/trigger/{{webhook_event}}/with/key/{{webhook_key}}?value1=test+action"
         )
 
-    def test_send_webhook_value(self, test_signal):
+    def test_send_webhook_value(self, fake_signal):
         assert (
-            test_signal.send_webhook("test+action")
+            fake_signal.send_webhook("test+action")
             == b"GET /trigger/{{webhook_event}}/with/key/{{webhook_key}}?value1=test+action HTTP/1.1\r\nHost: maker.ifttt.com\r\n\r\n"
         )
 
-    def test_send_webhook_type(self, test_signal):
-        assert type(test_signal.send_webhook("test+action")) == bytes
+    def test_send_webhook_type(self, fake_signal):
+        assert type(fake_signal.send_webhook("test+action")) == bytes
 
-    def test_first_signal_sends_webhook(self, test_signal):
+    def test_first_signal_sends_webhook(self, fake_signal, set_action1):
         assert (
-            test_signal.check_signal(True)
+            fake_signal.check_signal(1)
             == b"GET /trigger/{{webhook_event}}/with/key/{{webhook_key}}?value1=signal+activated HTTP/1.1\r\nHost: maker.ifttt.com\r\n\r\n"
         )
 
-    def test_second_signal_does_not_send_webhook(self, test_signal, set_signal_true):
-        # send second test_signal signal
-        assert test_signal.check_signal(True) is None
+    def test_second_signal_does_not_send_webhook(self, fake_signal, set_signal_true):
+        # send second fake_signal signal
+        assert fake_signal.check_signal(1) is None
 
-    def test_sigal_deactivation_sends_ceased_response(self, test_signal, set_signal_true):
-        assert test_signal.check_signal(False) == "signal+activated ceased"
+    def test_sigal_deactivation_sends_ceased_response(
+        self, fake_signal, set_signal_true
+    ):
+        assert fake_signal.check_signal(False) == "signal+activated ceased"
 
     def test_signal_deactivation_sends_action2_response_when_action2_exists(
-        self, test_signal, set_signal_true, set_action1, set_action2
+        self, fake_signal, set_signal_true, set_action1, set_action2
     ):
-        test_signal.check_signal(True)
+        fake_signal.check_signal(True)
         assert (
-            test_signal.check_signal(False)
+            fake_signal.check_signal(False)
             == b"GET /trigger/{{webhook_event}}/with/key/{{webhook_key}}?value1=signal+unactivated HTTP/1.1\r\nHost: maker.ifttt.com\r\n\r\n"
         )
 
