580cafd5c34e47ce2f0d19ae0a0b7f8899e1bbcf
ushills <web@ushills.co.uk>
2019-06-07 15:05:32 +0100

    remove mock_send_webhook from class

--- tests/test_notify_class.py
+++ tests/test_notify_class.py
@@ -16,52 +16,50 @@ sys.modules["usocket"] = mock_usocket
 from main import Notifier
 
 
-class WebhookMock:
-    def mock_send_webhook(self, action):
-        print("mocking send_webhook")
-        url = (
-            "http://maker.ifttt.com/trigger/{{webhook_event}}/with/key/{{webhook_key}}?value1="
-            + action
-        )
-        _, _, host, path = url.split("/", 3)
-        full_url = "GET /{} HTTP/1.1\r\nHost: {}\r\n\r\n".format(path, host).encode()
-        return full_url
+def mock_send_webhook(self, action):
+    print("mocking send_webhook")
+    url = (
+        "http://maker.ifttt.com/trigger/{{webhook_event}}/with/key/{{webhook_key}}?value1="
+        + action
+    )
+    _, _, host, path = url.split("/", 3)
+    full_url = "GET /{} HTTP/1.1\r\nHost: {}\r\n\r\n".format(path, host).encode()
+    return full_url
 
 
+@patch.object(Notifier, "send_webhook", mock_send_webhook)
 class TestNotifier:
     @pytest.fixture(scope="class")
     def fake_signal(self):
         return Notifier()
 
     @pytest.fixture(scope="class")
-    @patch.object(Notifier, "send_webhook", WebhookMock.mock_send_webhook)
+    @patch.object(Notifier, "send_webhook", mock_send_webhook)
     def set_action1(self, fake_signal):
         fake_signal.set_action1("signal+activated")
         assert fake_signal.action1 == "signal+activated"
 
     @pytest.fixture(scope="class")
-    @patch.object(Notifier, "send_webhook", WebhookMock.mock_send_webhook)
+    @patch.object(Notifier, "send_webhook", mock_send_webhook)
     def set_action2(self, fake_signal):
         fake_signal.set_action2("signal+unactivated")
         assert fake_signal.action2 == "signal+unactivated"
 
     @pytest.fixture(scope="class")
-    @patch.object(Notifier, "send_webhook", WebhookMock.mock_send_webhook)
+    @patch.object(Notifier, "send_webhook", mock_send_webhook)
     def set_signal_true(self, fake_signal):
         fake_signal.check_signal(1)
 
     @pytest.fixture(scope="class")
-    @patch.object(Notifier, "send_webhook", WebhookMock.mock_send_webhook)
+    @patch.object(Notifier, "send_webhook", mock_send_webhook)
     def set_signal_false(self, fake_signal):
         fake_signal.check_signal(0)
 
-    @patch.object(Notifier, "send_webhook", WebhookMock.mock_send_webhook)
     def test_signal_true_gives_signal_is_active(
         self, fake_signal, set_action1, set_signal_true
     ):
         assert fake_signal.signal_is_active is True
 
-    @patch.object(Notifier, "send_webhook", WebhookMock.mock_send_webhook)
     def test_signal_false_gives_signal_is_not_active(
         self, fake_signal, set_action1, set_signal_false
     ):
@@ -73,39 +71,30 @@ class TestNotifier:
             == "http://maker.ifttt.com/trigger/{{webhook_event}}/with/key/{{webhook_key}}?value1=test+action"
         )
 
-    def test_send_webhook_value_with_patch_context(self, fake_signal):
-        with patch.object(Notifier, "send_webhook"):
-            fake_signal.send_webhook("test+action")
-            fake_signal.send_webhook.assert_called_with("test+action")
+    def test_send_webhook_value(self, fake_signal):
+        assert (
+            fake_signal.send_webhook(fake_signal.action1)
+            == b"GET /trigger/{{webhook_event}}/with/key/{{webhook_key}}?value1=signal+activated HTTP/1.1\r\nHost: maker.ifttt.com\r\n\r\n"
+        )
 
-    @patch.object(Notifier, "send_webhook")
-    def test_send_webhook_value_with_patch_decorator(self, fake_signal):
-        fake_signal.send_webhook("test+action")
-        fake_signal.send_webhook.assert_called_with("test+action")
-
-    @patch.object(Notifier, "send_webhook", WebhookMock.mock_send_webhook)
     def test_send_webhook_type(self, fake_signal):
         assert type(fake_signal.send_webhook("test+action")) == bytes
 
-    @patch.object(Notifier, "send_webhook", WebhookMock.mock_send_webhook)
     def test_first_signal_sends_webhook(self, fake_signal):
         assert (
             fake_signal.check_signal(1)
             == b"GET /trigger/{{webhook_event}}/with/key/{{webhook_key}}?value1=signal+activated HTTP/1.1\r\nHost: maker.ifttt.com\r\n\r\n"
         )
 
-    @patch.object(Notifier, "send_webhook", WebhookMock.mock_send_webhook)
     def test_second_signal_does_not_send_webhook(self, fake_signal, set_signal_true):
         # send second fake_signal signal
         assert fake_signal.check_signal(1) is None
 
-    @patch.object(Notifier, "send_webhook", WebhookMock.mock_send_webhook)
     def test_sigal_deactivation_sends_ceased_response(
         self, fake_signal, set_signal_true
     ):
         assert fake_signal.check_signal(False) == "signal+activated ceased"
 
-    @patch.object(Notifier, "send_webhook", WebhookMock.mock_send_webhook)
     def test_signal_deactivation_sends_action2_response_when_action2_exists(
         self, fake_signal, set_signal_true, set_action1, set_action2
     ):
